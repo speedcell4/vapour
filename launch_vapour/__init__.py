c00b9b8fffddf0b134bec3ebb26d961e0468194a
speedcell4 <speedcell4@gmail.com>
2018-04-16 10:59:12 +0900

    ✨ use_cudnn and cudnn_deterministic

--- launch_vapour/__init__.py
+++ launch_vapour/__init__.py
@@ -3,7 +3,8 @@ import warnings
 import chainer
 
 
-def launch_vapour(device: int = -1, random_seed: int = 333) -> None:
+def launch_vapour(device: int = -1, random_seed: int = 333,
+                  use_cudnn: bool = True, cudnn_deterministic: bool = False) -> None:
     import numpy
     import random
 
@@ -19,3 +20,9 @@ def launch_vapour(device: int = -1, random_seed: int = 333) -> None:
         except Exception as e:
             warnings.warn(f'launch_vapour :: {e}')
     print("random seed: {}".format(random_seed), file=chainer.config.stdlog)
+
+    if use_cudnn:
+        chainer.config.use_cudnn = 'always'
+        chainer.config.cudnn_deterministic = cudnn_deterministic
+    else:
+        chainer.config.use_cudnn = 'never'
